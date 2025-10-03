b3aaf524d52514ae44bfb7b63ccf1f388c47b749
Daria Rosen <daria.sk135@gmail.com>
2025-10-03 14:25:48 +0300

    Change mongo access url

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,7 @@
 
 export default {
-    dbURL: process.env.MONGO_URL || 'mongodb+srv://reuven2408_db_user:airdnd321@cluster0.sp6ckzm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    dbName: process.env.DB_NAME || 'airdndDb'
+    // dbURL: process.env.MONGO_URL || 'mongodb+srv://reuven2408_db_user:airdnd321@cluster0.sp6ckzm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    // dbName: process.env.DB_NAME || 'airdndDb'
+    dbURL: process.env.MONGO_URL || 'mongodb+srv://DariaRosen:Daria13579@cluster0.3prj10p.mongodb.net/',
+    dbName: process.env.DB_NAME || 'AirDnD'
 }
